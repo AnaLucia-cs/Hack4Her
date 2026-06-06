23b58eab3cba0ca7d2ac2eaddb2967f5d2d75ab0
Ana Lucia Alonso Martínez <lucia.alonsom@uanl.edu.mx>
2026-06-06 16:38:05 -0600

    home page update

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -27,13 +27,20 @@ mongodb_uri = os.getenv('MONGODB_URI') or os.getenv('MONGODB_ATLAS_URI')
 mongo_client = MongoClient(mongodb_uri, serverSelectionTimeoutMS=3000) if mongodb_uri else None
 mongo_db = mongo_client[os.getenv('MONGODB_DB')] if mongo_client and os.getenv('MONGODB_DB') else None
 
+
+
 @app.route('/api/hello')
 def hello():
     return jsonify(message="¡Hola desde Flask!")
 
 @app.route('/')
 def index():
-  return render_template("home.html")
+    clientes_riesgo = 125
+
+    return render_template(
+        "home.html",
+        clientes_riesgo=clientes_riesgo,
+    )
 
 @app.route('/dashboards')
 @app.route('/Dashboards')
